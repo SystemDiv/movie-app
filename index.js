a50eed5702809e6540913718d07c69f480cdff71
S.M.Kashif <kashi_rock82@yahoo.com>
2020-11-30 15:25:06 +0500

    config changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(express.static(__dirname+'/dist'));
-app.get('/*', function (req, res) {
+app.get(config.app.clientRoutes, function (req, res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
