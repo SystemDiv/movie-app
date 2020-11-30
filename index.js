63ba4e7dd4fe3a79508944ff7c34389047c56fa2
S.M.Kashif <kashi_rock82@yahoo.com>
2020-11-30 16:26:58 +0500

    set path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var cors = require('cors');
 const express = require('express');
 var bodyParser = require('body-parser');
 const router = require('./routes/routes');
+;var path = require('path');
 var mongoose = require( 'mongoose' );
 var config = require('./config/config');
 const app = express();
@@ -9,10 +10,12 @@ const port = 8080;
 
 app.use(cors());
 app.options('*', cors());
+
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(express.static(__dirname+'/dist'));
+
 app.get(config.app.clientRoutes, function (req, res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
